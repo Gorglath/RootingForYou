28b786705b4fd1923aa770d29ab4c9e0373feeed
Zaatar <hani.zaatar.92@gmail.com>
2023-02-04 23:48:02 +0100

    Initial work on controller support

--- RootingForYou/Assets/Project/Scripts/UI/MainMenu.cs
+++ RootingForYou/Assets/Project/Scripts/UI/MainMenu.cs
@@ -2,8 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 public class MainMenu : MonoBehaviour
 {
+    public GameObject mainMenu, optionsMenu, creditsMenu;
+    public GameObject mainMenuFirstButton, optionsMenuFirstButton, creditsMenuFirstButton;
+    private void Update()
+    {
+
+    }
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
