cf0d3ba247fc5779919eeaa39b25cb41f9f82827
Zaatar <hani.zaatar.92@gmail.com>
2023-02-05 14:57:56 +0100

    Controller UI in UI_LevelPlaceholder

--- RootingForYou/Assets/Project/Scripts/UI/UIManager.cs
+++ RootingForYou/Assets/Project/Scripts/UI/UIManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 using TMPro;
 
@@ -8,7 +10,7 @@ using TMPro;
 public class UIManager : MonoBehaviour
 {
     private int score;
-    private float timer;
+    [SerializeField] private float timer;
     private int multiplier;
 
     public TMP_Text gameOverScoreText;
@@ -16,6 +18,20 @@ public class UIManager : MonoBehaviour
     public TMP_Text timerText;
 
     public Slider slider;
+
+    [Header("Parameters")]
+    [SerializeField] private PlayerInput m_playerInput = null;
+    [SerializeField] private string m_navigateActionName = null;
+    [SerializeField] private string m_startActionName = null;
+    public GameObject pauseMenu, optionsMenu, gameOverMenu, twoDimensionalUI, HUD;
+    [SerializeField] List<GameObject> pauseMenuButtons, optionsMenuButtons, gameOverMenuButtons;
+    public int pauseMenuButtonIndex, optionsMenuButtonIndex, gameOverMenuButtonIndex = 0;
+    private int menuIndex = 0;
+    private bool pausePressed = false;
+    private bool pauseUpdatedThisFrame = false;
+
+    Vector2 NavigationInput;
+    bool didReset = false;
 
     public int getScore() { return score; }
     public float getTimer() { return timer; }
@@ -30,11 +46,124 @@ public class UIManager : MonoBehaviour
 
     private void Update()
     {
-        /*if (Input.GetKeyDown(KeyCode.Space))
-            setScore(score + 10);*/
+        NavigationInput = m_playerInput.actions[m_navigateActionName].ReadValue<Vector2>();
+        if (m_playerInput.actions[m_startActionName].WasPressedThisFrame() && !pausePressed
+            && !pauseUpdatedThisFrame)
+        {
+            pausePressed = true;
+            pauseUpdatedThisFrame = true;
+            LoadPauseMenu();
+        }
+        if(m_playerInput.actions[m_startActionName].WasPressedThisFrame() && pausePressed
+            && !pauseUpdatedThisFrame)
+        {
+            pausePressed = false;
+            pauseUpdatedThisFrame = true;
+            UnloadPauseMenu();
+        }
+
+        NavigationInput = m_playerInput.actions[m_navigateActionName].ReadValue<Vector2>();
+        if (NavigationInput.magnitude == 0)
+        {
+            didReset = true;
+            return;
+        }
+        if (!didReset)
+            return;
+        didReset = false;
+        if(twoDimensionalUI.activeInHierarchy)
+            NavigateMenu(NavigationInput.y);
+
         updateMultiplier();
         updateScoreText();
         updateTimer();
+        pauseUpdatedThisFrame = false;
+    }
+
+    public void NavigateMenu(float pNavigationMagnitude)
+    {
+        if (menuIndex == 0)
+        {
+            EventSystem.current.SetSelectedGameObject(null);
+            if (pNavigationMagnitude < 0)
+                pauseMenuButtonIndex++;
+            else
+                pauseMenuButtonIndex--;
+            if (pauseMenuButtonIndex < 0)
+                pauseMenuButtonIndex = 0;
+            if (pauseMenuButtonIndex > pauseMenuButtons.Count - 1)
+                pauseMenuButtonIndex = pauseMenuButtons.Count - 1;
+            EventSystem.current.SetSelectedGameObject(pauseMenuButtons[pauseMenuButtonIndex]);
+        }
+        if (menuIndex == 1)
+        {
+            EventSystem.current.SetSelectedGameObject(null);
+            if (pNavigationMagnitude < 0)
+                optionsMenuButtonIndex++;
+            else
+                optionsMenuButtonIndex--;
+            if (optionsMenuButtonIndex < 0)
+                optionsMenuButtonIndex = 0;
+            if (optionsMenuButtonIndex > optionsMenuButtons.Count - 1)
+                optionsMenuButtonIndex = optionsMenuButtons.Count - 1;
+            EventSystem.current.SetSelectedGameObject(optionsMenuButtons[optionsMenuButtonIndex]);
+        }
+        if (menuIndex == 2)
+        {
+            EventSystem.current.SetSelectedGameObject(null);
+            if (pNavigationMagnitude < 0)
+                gameOverMenuButtonIndex++;
+            else
+                gameOverMenuButtonIndex--;
+            if (gameOverMenuButtonIndex < 0)
+                gameOverMenuButtonIndex = 0;
+            if (gameOverMenuButtonIndex > gameOverMenuButtons.Count - 1)
+                gameOverMenuButtonIndex = gameOverMenuButtons.Count - 1;
+            EventSystem.current.SetSelectedGameObject(gameOverMenuButtons[gameOverMenuButtonIndex]);
+        }
+    }
+
+    public void LoadOptionsMenu()
+    {
+        pauseMenuButtonIndex = 0;
+        gameOverMenuButtonIndex = 0;
+        menuIndex = 1;
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(optionsMenuButtons[optionsMenuButtonIndex]);
+    }
+
+    public void LoadPauseMenuFromOtherMenu()
+    {
+        optionsMenuButtonIndex = 0;
+        gameOverMenuButtonIndex = 0;
+        menuIndex = 0;
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(pauseMenuButtons[pauseMenuButtonIndex]);
+
+    }
+
+    public void LoadCreditsMenu()
+    {
+        pauseMenuButtonIndex = 0;
+        optionsMenuButtonIndex = 0;
+        menuIndex = 2;
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(gameOverMenuButtons[gameOverMenuButtonIndex]);
+    }
+
+    private void LoadPauseMenu()
+    {
+        HUD.SetActive(false);
+        twoDimensionalUI.SetActive(true);
+        pauseMenu.SetActive(true);
+    }
+
+    public void UnloadPauseMenu()
+    {
+        pauseMenu.SetActive(false);
+        optionsMenu.SetActive(false);
+        twoDimensionalUI.SetActive(false);
+        HUD.SetActive(true);
     }
 
     public void setScore(int pScore)
