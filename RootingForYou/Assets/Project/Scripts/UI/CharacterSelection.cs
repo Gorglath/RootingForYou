f31e4110ffa459c4612cb3d6e22504e8995c3844
Zaatar <hani.zaatar.92@gmail.com>
2023-02-05 15:56:23 +0100

    CharacterSelection menu Controller support, buttons need to be updated and horizontal swiping zeroes out main menu index for some reason

--- RootingForYou/Assets/Project/Scripts/UI/CharacterSelection.cs
+++ RootingForYou/Assets/Project/Scripts/UI/CharacterSelection.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
@@ -23,14 +25,161 @@ public class CharacterSelection : MonoBehaviour
     public List<Sprite> CosmeticSprites;
     public List<Sprite> CharacterNames;
 
+    public List<GameObject> CharacterSelectionScreenButtons;
+    private int CharacterSelectionButtonIndex;
+
+    Vector2 NavigationInput;
+    bool didReset = false;
+
+    [Header("Parameters")]
+    [SerializeField] private PlayerInput m_playerInput = null;
+    [SerializeField] private string m_navigateActionName = null;
+
+    public List<GameObject> PlayerOneHeadButtons;
+    public List<GameObject> PlayerOneCharacterButtons;
+    private int PlayerOneHeadButtonIndex;
+    private int PlayerOneCharacterButtonIndex;
+
+    public List<GameObject> PlayerTwoHeadButtons;
+    public List<GameObject> PlayerTwoCharacterButtons;
+    private int PlayerTwoHeadButtonIndex;
+    private int PlayerTwoCharacterButtonIndex;
+
+    /*public List<GameObject> PlayerOneVerticalButtons;
+    public GameObject PlayerOneHeadPreviousButton;
+    public GameObject PlayerOneCharacterPreviousButton;
+    private int PlayerOneButtonIndex;
+
+    public List<GameObject> PlayerTwoVerticalButtons;
+    public GameObject PlayerTwoHeadPreviousButton;
+    public GameObject PlayerTwoCharacterPreviousButton;
+    private int PlayerTwoButtonIndex;*/
+
+
     private void Start()
     {
-        InitCharacterSprites();
+        //InitCharacterSprites();
+    }
+    private void Update()
+    {
+        NavigationInput = m_playerInput.actions[m_navigateActionName].ReadValue<Vector2>();
+        if (NavigationInput.magnitude == 0)
+        {
+            didReset = true;
+            return;
+        }
+        if (!didReset)
+            return;
+        didReset = false;
+        NavigateMenuVertically(NavigationInput.y);
+        //NavigateMenuHorizontally(NavigationInput.x);
     }
 
     public void LoadMainMenu()
     {
         SceneManager.LoadScene(0);
+    }
+
+    public void NavigateMenuHorizontally(float pNavigationMagnitude)
+    {
+
+        EventSystem.current.SetSelectedGameObject(null);
+        if (CharacterSelectionButtonIndex == 0)
+        {
+            if (pNavigationMagnitude < 0)
+                PlayerOneHeadButtonIndex++;
+            else
+                PlayerOneHeadButtonIndex--;
+
+            if (PlayerOneHeadButtonIndex < 0)
+                PlayerOneHeadButtonIndex = 0;
+            if (PlayerOneHeadButtonIndex > PlayerOneHeadButtons.Count - 1)
+                PlayerOneHeadButtonIndex = PlayerOneHeadButtons.Count - 1;
+
+            EventSystem.current.SetSelectedGameObject(
+            PlayerOneHeadButtons[PlayerOneHeadButtonIndex]);
+            return;
+        }
+        
+        if(CharacterSelectionButtonIndex == 1)
+        {
+            if (pNavigationMagnitude < 0)
+                PlayerOneCharacterButtonIndex++;
+            else
+                PlayerOneCharacterButtonIndex--;
+
+            if (PlayerOneCharacterButtonIndex < 0)
+                PlayerOneCharacterButtonIndex = 0;
+            if (PlayerOneCharacterButtonIndex > PlayerOneCharacterButtons.Count - 1)
+                PlayerOneCharacterButtonIndex = PlayerOneCharacterButtons.Count - 1;
+
+            EventSystem.current.SetSelectedGameObject(
+            PlayerOneCharacterButtons[PlayerOneCharacterButtonIndex]);
+            return;
+        }
+        
+        if(CharacterSelectionButtonIndex == 2)
+        {
+            if (pNavigationMagnitude < 0 && CharacterSelectionButtonIndex == 2)
+                PlayerTwoHeadButtonIndex++;
+            else
+                PlayerTwoHeadButtonIndex--;
+
+            if (PlayerTwoHeadButtonIndex < 0)
+                PlayerTwoHeadButtonIndex = 0;
+            if (PlayerTwoHeadButtonIndex > PlayerTwoHeadButtons.Count - 1)
+                PlayerTwoHeadButtonIndex = PlayerTwoHeadButtons.Count - 1;
+
+            EventSystem.current.SetSelectedGameObject(
+                PlayerTwoHeadButtons[PlayerTwoHeadButtonIndex]);
+            return;
+        }
+        
+        if(CharacterSelectionButtonIndex == 3)
+        {
+            if (pNavigationMagnitude < 0 && CharacterSelectionButtonIndex == 3)
+                PlayerTwoCharacterButtonIndex++;
+            else
+                PlayerTwoCharacterButtonIndex--;
+
+            if (PlayerTwoCharacterButtonIndex < 0)
+                PlayerTwoCharacterButtonIndex = 0;
+            if (PlayerTwoCharacterButtonIndex > PlayerTwoCharacterButtons.Count - 1)
+                PlayerTwoCharacterButtonIndex = PlayerTwoCharacterButtons.Count - 1;
+
+            EventSystem.current.SetSelectedGameObject(
+                PlayerTwoCharacterButtons[PlayerTwoCharacterButtonIndex]);
+            return;
+        }
+    }
+
+    public void NavigateMenuVertically(float pNavigationMagnitude)
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        if (pNavigationMagnitude < 0)
+            CharacterSelectionButtonIndex++;
+        else
+            CharacterSelectionButtonIndex--;
+        
+        if (CharacterSelectionButtonIndex == 0)
+            PlayerOneHeadButtonIndex = 0;
+
+        if (CharacterSelectionButtonIndex == 1)
+            PlayerOneCharacterButtonIndex = 0;
+
+        if (CharacterSelectionButtonIndex == 2)
+            PlayerTwoHeadButtonIndex = 0;
+
+        if (CharacterSelectionButtonIndex == 3)
+            PlayerTwoCharacterButtonIndex = 0;
+
+        if (CharacterSelectionButtonIndex < 0)
+            CharacterSelectionButtonIndex = 0;
+        if (CharacterSelectionButtonIndex > CharacterSelectionScreenButtons.Count - 1)
+            CharacterSelectionButtonIndex = CharacterSelectionScreenButtons.Count - 1;
+        EventSystem.current.SetSelectedGameObject(
+            CharacterSelectionScreenButtons[CharacterSelectionButtonIndex]);
+        Debug.Log("Current Button Index: " + CharacterSelectionButtonIndex);
     }
 
     public void StartGame()
